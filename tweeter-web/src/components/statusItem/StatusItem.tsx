83ddace8261ceae9de118b815744d6fca01102c6
Thomas Bean <47thomasj@gmail.com>
2026-09-17 18:19:36 -0600

    Status Item

--- tweeter-web/src/components/statusItem/StatusItem.tsx
+++ tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -1,7 +1,44 @@
-export default function StatusItem() {
+import { Link } from "react-router-dom";
+import Post from "./Post";
+
+interface StatusItemProps {
+    item: any;
+    featurePath: string;
+    navigateToUser: (event: React.MouseEvent) => void;
+}
+
+export default function StatusItem({ item, featurePath, navigateToUser }: StatusItemProps) {
     return (
-        <div>
-            <h1>Status Item</h1>
+        <div className="col bg-light mx-0 px-0">
+            <div className="container px-0">
+            <div className="row mx-0 px-0">
+                <div className="col-auto p-3">
+                <img
+                    src={item.user.imageUrl}
+                    className="img-fluid"
+                    width="80"
+                    alt="Posting user"
+                />
+                </div>
+                <div className="col">
+                <h2>
+                    <b>
+                    {item.user.firstName} {item.user.lastName}
+                    </b>{" "}
+                    -{" "}
+                    <Link
+                    to={`${featurePath}/${item.user.alias}`}
+                    onClick={navigateToUser}
+                    >
+                    {item.user.alias}
+                    </Link>
+                </h2>
+                {item.formattedDate}
+                <br />
+                <Post status={item} featurePath={featurePath} />
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
